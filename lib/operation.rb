6c94de8bf7664820e55326f39a13ea20eb379b63
alessiobortone2 <alessiobortone2@gmail.com>
2017-10-02 12:55:13 +0100

    First sample test passes

--- lib/operation.rb
+++ lib/operation.rb
@@ -1,2 +1,13 @@
+require 'date'
+
+# implements single operation
 class Operation
+  attr_reader :date, :credit, :debit, :balance, :user
+  def initialize(date = Date.today, credit = 0, debit = 0, balance = 0, user = 'anon')
+    @date = date
+    @credit = credit
+    @debit = debit
+    @balance = balance
+    @user = user
+  end
 end
