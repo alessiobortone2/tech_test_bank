dca288701782614ffe89e11d9c76924191cb99c8
alessiobortone2 <alessiobortone2@gmail.com>
2017-10-03 17:02:32 +0100

    Start simplifying account class

--- lib/account.rb
+++ lib/account.rb
@@ -2,22 +2,21 @@ require_relative './transaction.rb'
 
 # implements account
 class Account
-  attr_reader :user, :balance, :transactions
-  def initialize(user = 'anon', balance = 0, transactions = [])
-    @user = user
+  attr_reader :balance, :transactions
+  def initialize(balance = 0, transactions = [])
     @balance = balance
     @transactions = transactions
   end
 
   def top_up(money)
     @transaction = Transaction.new
-    @balance += @transaction.add(money)
+    @transaction.add(money)
     @transactions << @transaction
   end
 
   def withdraw(money)
     @transaction = Transaction.new
-    @balance -= @transaction.subtract(money)
+    @transaction.subtract(money)
     @transactions << @transaction
   end
 
