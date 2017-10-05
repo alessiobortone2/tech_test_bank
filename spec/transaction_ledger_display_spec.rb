273e6402314a234ae2bbfc4e92044b0bf15d00ce
alessiobortone2 <alessiobortone2@gmail.com>
2017-10-05 10:27:44 +0100

    Fixed date in test, now working again

--- spec/transaction_ledger_display_spec.rb
+++ spec/transaction_ledger_display_spec.rb
@@ -19,10 +19,10 @@ RSpec.describe TransactionLedgerDisplay do
     @transactionledger.top_up(1000)
     @transactionledger.top_up(2000)
     @transactionledger.withdraw(500)
-    expect{@transactionledgerdisplay.print_all}.to \
-    output("date || credit || debit || balance\n"\
-      "04/10/2017 ||  || 500.00 || 2500.00\n"\
-      "04/10/2017 || 2000.00 ||  || 3000.00\n"\
-      "04/10/2017 || 1000.00 ||  || 1000.00\n").to_stdout
+    expect { @transactionledgerdisplay.print_all }.to \
+      output("date || credit || debit || balance\n"\
+      "05/10/2017 ||  || 500.00 || 2500.00\n"\
+      "05/10/2017 || 2000.00 ||  || 3000.00\n"\
+      "05/10/2017 || 1000.00 ||  || 1000.00\n").to_stdout
   end
 end
